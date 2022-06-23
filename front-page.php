9250c9429a369add759d36387e797090094eb049
Cedric Forges <e2195286@cmaisonneuve.qc.ca>
2022-06-23 11:21:39 -0400

    Ajout de front-page.php

--- front-page.php
+++ front-page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?> 
-<h1>front-page.php Mon premier modèle Wordpress</h1>
+<h1>front-page.php Mon premier modèle Wordpress pour mon site</h1>
 <?php
 if (have_posts()):
     while(have_posts()) : the_post(); ?>
